58331197cd19b57fd65163db702e3206ae5860c5
bala <b.balasrisabhari@gmail.com>
2025-12-20 00:26:34 +0530

    Updated files

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+require("dotenv").config();
 
-dotenv.config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Database Connection
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/journalapp')
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, {
+})
+.then(() => console.log("MongoDB Atlas Connected 🚀"))
+.catch((err) => console.log("MongoDB Error ❌:", err));
+mongoose.connection.once("open", () => {
+  console.log("CONNECTED TO:", mongoose.connection.name);
+});
 
-app.get('/', (req, res) => {
-    res.send('Journal App API Running');
+
+// Default Route
+app.get("/", (req, res) => {
+  res.send("Journal App API Running");
 });
 
 // Routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/journals', require('./routes/journal'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/journals", require("./routes/journal"));
 
+// Server
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
